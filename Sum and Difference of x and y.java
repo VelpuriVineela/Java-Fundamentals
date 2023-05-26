9c5e20ee48bf39043eee6906562f5847c2ad498e
Velpuri Vineela <134683293+VelpuriVineela@users.noreply.github.com>
2023-05-26 13:58:45 +0530

    Create Sum and Difference of x and y.java

---
+++ Sum and Difference of x and y.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sem = new Scanner(System.in);
+        
+        int x = sem.nextInt();
+        int y = sem.nextInt();
+        
+        System.out.println(x+y);
+        System.out.println(x-y);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
