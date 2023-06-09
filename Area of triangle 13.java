b606d4a27aab35c8ece4c17765d93f8a567ba445
Velpuri Vineela <134683293+VelpuriVineela@users.noreply.github.com>
2023-06-09 14:21:42 +0530

    Create Area of triangle 13.java

---
+++ Area of triangle 13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sem = new Scanner(System.in);
+        
+        double x = sem.nextDouble();
+        double y = sem.nextDouble();
+        
+        double area = (1*x*y)/2;
+        
+        System.out.println(area);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
