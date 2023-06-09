b886a9964747fd0fa8c1561ef631a684e368e5f1
Velpuri Vineela <134683293+VelpuriVineela@users.noreply.github.com>
2023-06-09 14:18:19 +0530

    Create Fahrenheit and Celsius.java

---
+++ Fahrenheit and Celsius.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sem = new Scanner(System.in);
+        
+        double F = sem.nextDouble();
+        double C;
+        
+        C = ((F-32)*5)/9;
+        
+        System.out.println(C);
+        
+        
+        
+        
+       
+    }
+}
