7e0e208d839f3d8f69d335485b22855ccb06a5ec
Velpuri Vineela <134683293+VelpuriVineela@users.noreply.github.com>
2023-06-09 14:25:36 +0530

    Create Even or not.java

---
+++ Even or not.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sem = new Scanner(System.in);
+        
+        int number = sem.nextInt();
+        
+        System.out.println((number%2)==0);
+    }
+}
